e23ce5fe56b39920afc979d68ad5e20539155b62
Sean Coh <scoh21@basecampcodingacademy.org>
2021-03-08 16:13:53 -0600

    added about field

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 # Create your models here.
 class User(AbstractUser):
-    genre =  models.CharField()
+    about = models.TextField(max_length=2000)
+    genre = models.CharField()
     image = models.ImageField()
     role = (
         ('Listener', "Listener"),
