a56f8b64ec67a451e7421d62032b9d14dd6bd755
Sean Coh <scoh21@basecampcodingacademy.org>
2021-03-08 16:05:00 -0600

    created model for user

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-ROLE_CHOICES= [
-    ('', ''),
-]
 # Create your models here.
 class User(AbstractUser):
-    
+    genre =  models.CharField()
+    image = models.ImageField()
+    role = (
+        ('Listener', "Listener"),
+        ('Artist', "Artist")
+    )
+    roles = models.CharField(max_length=8,choices=role, default="Listener")
