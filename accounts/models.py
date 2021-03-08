4032ee0b811bc9f574c87df6a991518b163e8491
Bryce Taylor <btaylor21@basecampcodingacademy.org>
2021-03-08 16:10:50 -0600

    model update

--- accounts/models.py
+++ accounts/models.py
@@ -3,8 +3,12 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 ROLE_CHOICES= [
-    ('', ''),
+    ('listener', 'Listener'),
+    ('artist', 'Artist'),
 ]
 # Create your models here.
 class User(AbstractUser):
+    roles = forms.CharField(label='What role would you like to be?', widget=forms.Select(choices=ROLE_CHOICES))
+    fav_genre = models.CharField(max_length=200)
+    about = = models.TextField(max_length=500)
     
