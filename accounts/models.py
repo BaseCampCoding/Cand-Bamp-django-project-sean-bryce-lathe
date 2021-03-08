5c4f0c56fbf134bf4d57ca22ce863ef50d741f35
Bryce Taylor <btaylor21@basecampcodingacademy.org>
2021-03-08 15:48:49 -0600

    worked urls, settings, and models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+ROLE_CHOICES= [
+    ('', ''),
+]
 # Create your models here.
+class User(AbstractUser):
+    
--- config/urls.py
+++ config/urls.py
@@ -18,4 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("signup/", users.views.SignUpView.as_view(), name="signup"),
 ]
