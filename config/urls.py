448cf34bfd24358a69de81bc0d6b8179f6ef6f2a
Bryce Taylor <btaylor21@basecampcodingacademy.org>
2021-03-09 10:39:32 -0600

    Update forms & urls

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import accounts.views, cand-bamp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
