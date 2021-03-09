19ec066c5747d102bd099fa2b76b157b80ec414d
Lathe Ward <lward21@basecampcodingacademy.org>
2021-03-09 10:55:26 -0600

    changed AUTH_USER_MODEL to correct model user

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = 'accounts.User'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
