12ea27814cd5896b33d3cc516cfa5ac1565d8bb6
Lathe Ward <lward21@basecampcodingacademy.org>
2021-03-09 11:23:16 -0600

    Corrected model name

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,10 +4,10 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 class UserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
-        model = User
+        model = CustomUser
         fields = ('username', 'first_name', 'last_name', 'email', 'password', 'genre', 'image', 'roles', 'about',)
 
 class UserChangeForm(UserChangeForm):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ('username', 'first_name', 'last_name', 'email', 'password', 'genre', 'image', 'roles', 'about',)
