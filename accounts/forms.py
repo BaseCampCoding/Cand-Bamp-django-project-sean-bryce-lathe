9e7ffc6ce0f94058844559a45b4de6bead93291e
Lathe Ward <lward21@basecampcodingacademy.org>
2021-03-09 10:12:17 -0600

    Removed 'date_joined' from form

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 class UserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password', 'date_joined', 'genre', 'image', 'roles', 'about')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password', 'genre', 'image', 'roles', 'about')
 
 class UserChangeForm(UserChangeForm):
     class Meta:
