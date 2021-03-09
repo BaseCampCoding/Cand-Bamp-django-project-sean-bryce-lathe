29608403ea6d7bfb42cce93324d745dead7cc710
Bryce Taylor <btaylor21@basecampcodingacademy.org>
2021-03-09 11:33:13 -0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
-AUTH_USER_MODEL = 'accounts.User'
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
