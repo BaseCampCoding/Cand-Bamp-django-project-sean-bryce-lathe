9d5049fc8fb01e81d4c2cb858793c98cb2541023
Lathe Ward <lward21@basecampcodingacademy.org>
2021-03-09 14:49:32 -0600

    Commented out code for now, testing

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,9 @@
-from django.db import models
-from django.urls import reverse
+# from django.db import models
+# from django.urls import reverse
 
-# Create your models here.
-class Posts(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.foreig
-    body = ...
-    genre = ...
+# # Create your models here.
+# class Posts(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.foreig
+#     body = ...
+#     genre = ...
