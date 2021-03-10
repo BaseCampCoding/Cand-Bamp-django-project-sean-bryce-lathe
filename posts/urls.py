97e3aaaa87fc7cbcd77a1f71dc6b7ba2d3bfd4ae
Lathe Ward <lward21@basecampcodingacademy.org>
2021-03-10 10:06:47 -0600

    Set url patterns for Post

--- posts/urls.py
+++ posts/urls.py
@@ -1,2 +1,10 @@
 from django.urls import path
-from .views import ...
+from .views import (PostCreateView, PostDeleteView, PostDetailView, PostListView, PostUpdateView,)
+
+urlpatterns = [
+    path('post/<int:pk>/edit', PostUpdateView.as_view(), name='post_edit'),
+    path('post/new', PostCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post_delete'),
+    # path('', PostListView.as_view(), name='Artist_list'),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 # Create your views here.
 
@@ -16,3 +17,13 @@ class PostCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body', 'genre']
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body', 'genre']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
