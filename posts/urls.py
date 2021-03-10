db42e18ff451ce476f61e2b5f025a96239999102
Sean Coh <scoh21@basecampcodingacademy.org>
2021-03-10 15:59:23 -0600

    renamed the model to articlepost

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import (ArtistListView, PostCreateView, PostDeleteView, PostDetailView, PostListView, PostUpdateView,)
+from .views import (PostCreateView, PostDeleteView, PostDetailView, PostListView, PostUpdateView, HomePageView,)
 
 urlpatterns = [
-    path('post/<int:pk>/edit', PostUpdateView.as_view(), name='post_edit'),
-    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post_delete'),
-    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
-    path('post/new', PostCreateView.as_view(), name='post_new'),
-    path('', ArtistListView.as_view(), name='artist_list'),
+    path('post/<int:pk>/edit', ArticlePostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete', ArticlePostDeleteView.as_view(), name='post_delete'),
+    path('post/', ArticlePostListView.as_view(), name='article_list'),
+    path('post/<int:pk>', ArticlePostDetailView.as_view(), name='post_detail'),
+    path('post/new', ArticlePostCreateView.as_view(), name='post_new'),
+    path('', HomePageView.as_view(), name='home'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
+ 
 # Create your views here.
-class ArtistListView(ListView):
-    model = Post
-    template_name = 'artist_list.html'
-    
-class PostListView(ListView):
-    model = Post
+class HomePageView(TemplateView): 
     template_name = 'home.html'
+    
+class ArticlePostListView(ListView):
+    model = ArticlePost
+    template_name = 'artist_list.html'
 
-class PostDetailView(DetailView):
-    model = Post
+class ArticlePostDetailView(DetailView):
+    model = ArticlePost
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
-    model = Post
+class ArticlePostCreateView(CreateView):
+    model = ArticlePost
     template_name = 'posts/post_new.html'
     fields = ['title', 'body', 'genre']
 
@@ -26,12 +27,14 @@ class PostCreateView(CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(UpdateView):
-    model = Post
+class ArticlePostUpdateView(UpdateView):
+    model = ArticlePost
     template_name = 'post_edit.html'
     fields = ['title', 'body', 'genre']
 
-class PostDeleteView(DeleteView):
-    model = Post
+class ArticlePostDeleteView(DeleteView):
+    model = ArticlePost
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
+
+
