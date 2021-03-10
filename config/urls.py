eb27ada6e90d87caa331f20eaf561a59efd2d96c
Bryce Taylor <btaylor21@basecampcodingacademy.org>
2021-03-10 10:01:25 -0600

    Artist listview

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+import posts.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +24,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'),
         name='home'),
+    path('posts/', include('posts.urls')), 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from django.urls import reverse_lazy
+from .models import Posts
 # Create your views here.
+class ArtistListView(LoginRequiredMixin, ListView):
+    model = Posts
+    template_name = 'Artist_list.html'
