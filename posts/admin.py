29a58e50f2105166bc46ba2bfbc226c3ed1ff838
Bryce Taylor <btaylor21@basecampcodingacademy.org>
2021-03-12 09:14:26 -0600

    update admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import ArticlePost, Comment
 # Register your models here.
 
@@ -7,9 +6,3 @@ from .models import ArticlePost, Comment
 
 admin.site.register(ArticlePost)
 admin.site.register(Comment)
-=======
-from .models import ArticlePost
-
-# Register your models here.
-admin.site.register(ArticlePost)
->>>>>>> 50d0c04ba83972eb238b98e207e5d505777f0742
