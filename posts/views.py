9e00241088d559653756e1905572537b56ba063e
Lathe Ward <lward21@basecampcodingacademy.org>
2021-03-12 09:15:56 -0600

    For some reason I am seeing bryce's changes - Lathe

--- posts/views.py
+++ posts/views.py
@@ -5,7 +5,9 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy, reverse
 from django.http import HttpResponseRedirect
-from .models import ArticlePost
+from .models import ArticlePost, Comment
+from .forms import CommentForm
+
  
 # Create your views here.
 class HomePageView(TemplateView): 
@@ -27,6 +29,20 @@ class ArticlePostCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class AddCommentView(CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'add_comment.html'
+    
+    def form_valid(self, form):
+        # form.instance.author = self.request.user
+        temp_id = self.request.resolver_match.kwargs["pk"]
+        temp_obj = ArticlePost.objects.get(id=temp_id)
+        form.instance.post = temp_obj
+        return super().form_valid(form)
+    success_url = reverse_lazy('article_list')
+
 
 class ArticlePostUpdateView(UpdateView):
     model = ArticlePost
