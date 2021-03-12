e86f7d18096c1556348cd1d05e286a2bc343d721
Lathe Ward <lward21@basecampcodingacademy.org>
2021-03-12 09:22:02 -0600

    Removed git hiccup in code

--- posts/admin.py
+++ posts/admin.py
@@ -1,25 +1,6 @@
 from django.contrib import admin
-<<<<<<< HEAD
-<<<<<<< HEAD
 from .models import ArticlePost, Comment
 # Register your models here.
 
-
-
 admin.site.register(ArticlePost)
 admin.site.register(Comment)
-=======
-from .models import ArticlePost
-
-# Register your models here.
-admin.site.register(ArticlePost)
->>>>>>> 50d0c04ba83972eb238b98e207e5d505777f0742
-=======
-from .models import ArticlePost, Comment
-# Register your models here.
-
-
-
-admin.site.register(ArticlePost)
-admin.site.register(Comment)
->>>>>>> 29a58e50f2105166bc46ba2bfbc226c3ed1ff838
