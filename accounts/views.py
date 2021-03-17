21a98c66a97995d07f5c8ad27d6b04233478d4ab
Bryce-Taylor <72210125+Bryce-Taylor@users.noreply.github.com>
2021-03-17 15:37:49 -0500

    User can now edit profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy, reverse
 from django.http import HttpResponseRedirect
 from django.views.generic import CreateView
 from .forms import UserCreationForm
 from .models import CustomUser
-from posts.models import ArticlePost
+from posts.models import ArticlePost, Song
 import accounts
 
 # Create your views here.
@@ -32,11 +33,21 @@ class UserProfileDetailView(DetailView):
         found_user = CustomUser.objects.filter(id=cur_user_id).first()
         context['following'] = found_user.followers.filter(id=self.request.user.id).exists()
         print(found_user, context['following'], found_user.followers) 
-
+        context['Song_posts'] = Song.objects.filter(artist__id=cur_user_id)
         context['local_posts'] = ArticlePost.objects.filter(author__id=cur_user_id)
         context['roles'] = self.object.roles
         return context
 
+class UserProfileEdit(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = CustomUser
+    template_name = 'user_profile_edit.html'
+    fields = ['roles', 'profile_picture', 'genre', 'about']
+    
+    def get_success_url(self):
+        return reverse_lazy('user_profile', kwargs={'pk': self.object.pk})
+
+    def test_func(self): 
+        return self.get_object() == self.request.user
     
 def FollowView(request, pk):
     user_to_follow = get_object_or_404(CustomUser, id=pk)
